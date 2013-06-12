7713ccf5ce1a95ad3c567c0b2744235488cbc46d
mena.samy@gmail.com <mena.samy@gmail.com@2dfd42b7-506e-2593-5f8c-85cb37a3bbcf>
2013-06-12 10:06:45 +0000

    add master page

---
+++ trunk/I_Stock/I_Stock/I_Stock/Masterpages/IStock.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace I_Stock.Masterpages
+{
+    public partial class IStock : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
