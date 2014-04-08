5eeece72eb02d05ae44d678e45ab13b3b98b91f2
M.Gherbas@gmail.com <M.Gherbas@gmail.com@2dfd42b7-506e-2593-5f8c-85cb37a3bbcf>
2014-04-08 18:02:59 +0000

    Add missing files MembershipBLL

---
+++ trunk/Chat2Connect/BLL/MembershipBLL.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace BLL
+{
+    public class MembershipBLL : _Membership
+    {
+        public bool RegisterMember(string userName, string password, string email, string question, string questionAnswer, out MembershipCreateStatus createStatus)
+        {
+            MembershipUser objUser = Membership.CreateUser(userName, password, email, question, questionAnswer, true, out createStatus);
+            if (createStatus == MembershipCreateStatus.Success)
+            {
+                BLL.Member member = new BLL.Member();
+                member.AddNew();
+                member.Answer = questionAnswer;
+                member.UserID = new Guid(objUser.ProviderUserKey.ToString());
+                member.Name = objUser.UserName;
+                member.IsOnLine = true;
+                member.Status = 1;
+                member.Save();
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
