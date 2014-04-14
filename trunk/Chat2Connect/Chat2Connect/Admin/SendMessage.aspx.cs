e2f06d7105041d36184d790c4e2fe2de962b53e9
M.Gherbas@gmail.com <M.Gherbas@gmail.com@2dfd42b7-506e-2593-5f8c-85cb37a3bbcf>
2014-04-14 23:25:57 +0000

    Add Send message user control and use it in admin page (Send message).

--- trunk/Chat2Connect/Chat2Connect/Admin/SendMessage.aspx.cs
+++ trunk/Chat2Connect/Chat2Connect/Admin/SendMessage.aspx.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -21,7 +23,13 @@ namespace Chat2Connect.Admin
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                Member member = new Member();
+                member.GetMemberByUserId(new Guid(Membership.GetUser().ProviderUserKey.ToString()));
+                ctrlSendMail.MemberID = member.MemberID;
+                ctrlSendMail.MemberName = member.Name;
+            }
         }
     }
 }
