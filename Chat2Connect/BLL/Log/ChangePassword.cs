0a64d0b0a8a9b5f71b595b0a0e5d0cced79298ba
M.Gherbas <M.Gherbas@gmail.com>
2014-05-15 21:33:59 +0000

    Add logging missing files.

---
+++ Chat2Connect/BLL/Log/ChangePassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Log
+{
+    public class ChangePassword:Log
+    {
+        public ChangePassword()
+        {
+            Type = Helper.Enums.LogType.ChangePassword;
+        }
+
+        public string OldPassword
+        {
+            get;
+            set;
+        }
+        public string NewPassword
+        {
+            get;
+            set;
+        }
+    }
+}
