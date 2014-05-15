ca87103a5d66c9e61a43df1f9b187a1632decbcc
M.Gherbas@gmail.com <M.Gherbas@gmail.com@2dfd42b7-506e-2593-5f8c-85cb37a3bbcf>
2014-05-15 21:33:59 +0000

    Add logging missing files.

---
+++ trunk/Chat2Connect/BLL/Log/Log.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.Log
+{
+    public class Log
+    {
+        [JsonIgnore]
+        public Helper.Enums.LogType Type
+        {
+            get;
+            set;
+        }
+
+        public string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+
+        }
+
+        public T Deserialize<T>(string stringValue)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(stringValue);
+        }
+
+    }
+}
