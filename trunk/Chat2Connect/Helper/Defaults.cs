da96dfa90509904f6c14934980e9b5b28706b9ed
M.Gherbas@gmail.com <M.Gherbas@gmail.com@2dfd42b7-506e-2593-5f8c-85cb37a3bbcf>
2014-05-19 19:34:11 +0000

    add missing helper default file.

---
+++ trunk/Chat2Connect/Helper/Defaults.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helper
+{
+    public class Defaults
+    {
+        public static int MemberTypeSpecDurationID
+        {
+            get
+            {
+                int id = 1;
+                if (System.Configuration.ConfigurationManager.AppSettings["DefaultMemberTypeSpecDurationID"] != null)
+                    id = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["DefaultMemberTypeSpecDurationID"]);
+                return id;
+            }
+        }
+
+        public static int RoomTypeSpecDurationID
+        {
+            get
+            {
+                int id = 1;
+                if (System.Configuration.ConfigurationManager.AppSettings["DefaultRoomTypeSpecDurationID"] != null)
+                    id = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["DefaultRoomTypeSpecDurationID"]);
+                return id;
+            }
+        }
+        public static int VIPRoomTypeSpecID
+        {
+            get
+            {
+                int id = 4;
+                if (System.Configuration.ConfigurationManager.AppSettings["VIPRoomTypeSpecID"] != null)
+                    id = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["VIPRoomTypeSpecID"]);
+                return id;
+            }
+        }
+    }
+}
