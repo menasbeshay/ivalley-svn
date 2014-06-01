c121325a10bf877974d5c85c2370a0708362ea16
M.Gherbas <M.Gherbas@gmail.com>
2014-06-01 18:34:22 +0000

    Add missing file.

---
+++ Chat2Connect/Helper/TypeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helper
+{
+    public class TypeConverter
+    {
+        public static bool ToBoolean(object val)
+        {
+            if (val == DBNull.Value)
+                return false;
+            return Convert.ToBoolean(val);
+        }
+
+        public static int ToInt32(object val)
+        {
+            if (val == DBNull.Value)
+                return 0;
+            return Convert.ToInt32(val);
+        }
+
+        public static string ToString(object val)
+        {
+            if (val == DBNull.Value)
+                return String.Empty;
+            return Convert.ToString(val);
+        }
+    }
+}
