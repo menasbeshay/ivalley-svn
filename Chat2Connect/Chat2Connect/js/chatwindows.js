2d93bd0cf1175f7acbccaa6b74485127c306a1df
M.Gherbas <M.Gherbas@gmail.com>
2014-06-13 17:11:13 +0000

    Date formate in room messages history

--- Chat2Connect/Chat2Connect/js/chatwindows.js
+++ Chat2Connect/Chat2Connect/js/chatwindows.js
@@ -10,6 +10,25 @@ ko.bindingHandlers.slideVisible = {
     }
 };
 
+ko.bindingHandlers.date = {
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = valueAccessor();
+        var allBindings = allBindingsAccessor();
+        var valueUnwrapped = ko.utils.unwrapObservable(value);
+        // Date formats: http://momentjs.com/docs/#/displaying/format/
+        var pattern = allBindings.format || 'DD/MM/YYYY';
+        var output = "-";
+        if (valueUnwrapped !== null && valueUnwrapped !== undefined && valueUnwrapped.length > 0) {
+            output = moment(valueUnwrapped).format(pattern);
+        }
+
+        if ($(element).is("input") === true) {
+            $(element).val(output);
+        } else {
+            $(element).text(output);
+        }
+    }
+};
 function Chat(maxWin, memberID, memberName) {
     var self = this;
     self.CurrentMemberID = memberID;
