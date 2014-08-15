f6e79693fd4569d2519d66c013164d24df440f05
M.Gherbas@gmail.com <M.Gherbas@gmail.com@2dfd42b7-506e-2593-5f8c-85cb37a3bbcf>
2014-08-15 17:29:48 +0000

    Rooms report.

--- trunk/Chat2Connect/scripts/15-8-2014.sql
+++ trunk/Chat2Connect/scripts/15-8-2014.sql
@@ -28,3 +28,29 @@ BEGIN
 	ORDER BY m.CreateDate DESC 
 	
 END
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[proc_RoomSearch]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [dbo].[proc_RoomSearch]
+GO
+CREATE PROCEDURE [proc_RoomSearch]
+(
+	@CreatedDateFrom DATETIME = NULL,
+	@CreatedDateTo DATETIME = NULL,
+	@RoomStatus INT = NULL
+)
+AS
+BEGIN
+	SELECT r.*,[OwnerName]=u.UserName
+	FROM Room r
+	LEFT JOIN Member m on m.MemberID=r.CreatedBy
+	LEFT JOIN aspnet_Users u on u.UserId=m.UserId
+	
+	WHERE 
+	(@CreatedDateFrom IS NULL OR CONVERT(date, r.CreatedDate)>=@CreatedDateFrom)
+	AND (@CreatedDateTo IS NULL OR CONVERT(date, r.CreatedDate)<=@CreatedDateTo)
+	AND (@RoomStatus IS NULL OR r.RowStatusID=@RoomStatus)
+	
+	ORDER BY r.CreatedDate DESC 
+	
+END
