3088dc18a52bc062f3a2c6dee4d45b9951fda659
M.Gherbas <M.Gherbas@gmail.com>
2014-10-13 19:00:20 +0000

    mail ads page.

---
+++ Chat2Connect/Chat2Connect/Admin/MailAds.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.master" AutoEventWireup="true" CodeBehind="MailAds.aspx.cs" Inherits="Chat2Connect.Admin.MailAds" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderhead" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <input type="hidden" runat="server" id="adID" value="0" />
+    <h1>الإعلانات فى صندوق البريد</h1>
+    <div class="form-group pull-right">
+        <asp:Label ID="lblTitle" runat="server" CssClass="form-control" Text="إعلان جديد"></asp:Label>
+    </div>
+    <div class="form-horizontal blockBox">
+        <div class="form-group">
+            <div class="pull-right col-lg-5">
+                <div class="form-group">
+                    <div class="col-sm-4 control-label pull-right">
+                        <label>نص الإعلان</label>
+                    </div>
+                    <div class="col-sm-8 pull-right">
+                        <asp:TextBox ID="txtAdText" runat="server" TextMode="MultiLine" MaxLength="500" CssClass="form-control"> </asp:TextBox>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-4 control-label pull-right">
+                        <label>إختر الصورة</label>
+                    </div>
+                    <div class="col-sm-8 pull-right">
+                        <asp:FileUpload ID="uiFileUploadAddImage" runat="server" />
+                    </div>
+                </div>
+            </div>
+            <div class="pull-right col-lg-7">
+                <div class="form-group">
+                    <div class="pull-right col-lg-4">
+                        نوع الحسابات
+                <asp:CheckBoxList ID="chkMemberTypes" runat="server"></asp:CheckBoxList>
+                    </div>
+                    <div class="pull-right col-lg-4">
+                        المدة بالأيام
+                <asp:DropDownList ID="lstDays" runat="server"></asp:DropDownList>
+                    </div>
+                    <div class="pull-right col-lg-4">
+                        عدد مرات الإعلان فى اليوم
+                <asp:DropDownList ID="lstHours" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-lg-2 pull-right">
+                        تاريخ بدء الإعلان
+                    </div>
+                    <div class="col-lg-2 pull-right">
+                        <asp:TextBox ID="txtStartDate" runat="server" CssClass="form-control datecontrol"></asp:TextBox>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-lg-2 pull-right">
+                        حالة الإعلان
+                    </div>
+                    <div class="col-lg-10 pull-right">
+                        <asp:RadioButtonList ID="rdStatus" runat="server" CssClass="form-control"></asp:RadioButtonList>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <asp:Button ID="btnSaveAd" runat="server" CssClass="btn btn-warning" Text="حفظ" OnClick="btnSaveAd_Click" />
+        </div>
+    </div>
+    <table class="table table-condensed">
+        <tr>
+            <th>رقم الإعلان</th>
+            <th>الحالة</th>
+            <th>نص الإعلان</th>
+        </tr>
+        <asp:Repeater ID="repAds" runat="server">
+            <ItemTemplate>
+                <tr>
+                    <td>
+                        <asp:LinkButton ID="lnkEdit" runat="server" CommandArgument='<%# Eval("ID") %>' OnClick="lnkEdit_Click"></asp:LinkButton></td>
+                    <td><%# GetStatusName(Eval("RowStatusID")) %></td>
+                    <td>
+                        <%# GetAdContent(Eval("AdText"),Eval("AdImage")) %>
+                    </td>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+</asp:Content>
---
+++ Chat2Connect/Chat2Connect/Admin/MailAds.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Chat2Connect.Admin
+{
+    public partial class MailAds : System.Web.UI.Page
+    {
+        public static List<string> PageRoles
+        {
+            get
+            {
+                List<string> lstRoles = new List<string>();
+                lstRoles.Add(Helper.Enums.AdminRoles.Admin_MailAds.ToString());
+                return lstRoles;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtStartDate.Text = Helper.Date.ToString(DateTime.Now);
+
+                BLL.MailAds ads = new BLL.MailAds();
+                ads.LoadAll();
+
+                repAds.DataSource = ads.DefaultView;
+                repAds.DataBind();
+
+                for (int i = 1; i <= 30;i++ )
+                {
+                    lstDays.Items.Add(new ListItem(i.ToString()));
+                }
+                for (int i = 1; i <= 24; i++)
+                {
+                    lstHours.Items.Add(new ListItem(i.ToString()));
+                }
+                var lstStatus = Helper.EnumUtil.GetValues<Helper.Enums.RowStatus>().Select(r => new
+                {
+                    ID = (int)r,
+                    Name = Helper.StringEnum.GetStringValue(r)
+                }).ToList();
+                rdStatus.DataValueField = "ID";
+                rdStatus.DataTextField = "Name";
+                rdStatus.DataSource = lstStatus;
+                rdStatus.DataBind();
+
+                var lstMemberTypes = Helper.EnumUtil.GetValues<Helper.Enums.MemberType>().Select(r => new
+                {
+                    ID = (int)r,
+                    Name = Helper.StringEnum.GetStringValue(r)
+                }).ToList();
+                chkMemberTypes.DataValueField = "ID";
+                chkMemberTypes.DataTextField = "Name";
+                chkMemberTypes.DataSource = lstMemberTypes;
+                chkMemberTypes.DataBind();
+            }
+
+        }
+
+        protected void btnSaveAd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = "~/" + Helper.Defaults.AdsPicFolder;
+                if (uiFileUploadAddImage.HasFile)
+                {
+                    path = path + "/" + DateTime.Now.ToString("ddMMyyyy_hhmmss_") + uiFileUploadAddImage.FileName;
+                    uiFileUploadAddImage.SaveAs(Server.MapPath(path));
+                }
+
+                BLL.MailAds bllMailAd = new BLL.MailAds();
+                bllMailAd.AddNew();
+                bllMailAd.Timestamp = DateTime.Now;
+                bllMailAd.AdText = txtAdText.Text;
+                bllMailAd.AdImage = path.Substring(1);
+                bllMailAd.RowStatusID = Convert.ToInt32(rdStatus.SelectedValue);
+                bllMailAd.NumOfDays = Convert.ToInt32(lstDays.SelectedValue);
+                bllMailAd.NuOfHours = Convert.ToInt32(lstHours.SelectedValue);
+                bllMailAd.StartDate = Helper.Date.ToDate(txtStartDate.Text);
+                string strMembers = "";
+                for (int i = 0; i < chkMemberTypes.Items.Count;i++ )
+                {
+                    if (chkMemberTypes.Items[i].Selected)
+                    {
+                        strMembers += "," + chkMemberTypes.Items[i].Value;
+                    }
+                }
+                bllMailAd.MemberTypes = strMembers;
+                bllMailAd.Save();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected void lnkEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public string GetStatusName(object statusID)
+        {
+            return Helper.StringEnum.GetStringValue(Helper.EnumUtil.ParseEnum<Helper.Enums.RowStatus>(Helper.TypeConverter.ToInt32(statusID)));
+        }
+
+        public static string GetAdContent(object adText,object adImage)
+        {
+            string result = "";
+            if (adText != DBNull.Value)
+            {
+                result = adText.ToString().Replace("\r\n", "</br>");
+                if (adImage != DBNull.Value)
+                {
+                    result += "</br><img src='" + adImage.ToString() + "'/>";
+                }
+            }
+            return result;
+        }
+        
+    }
+}
