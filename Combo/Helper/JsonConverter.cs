79a4500e08fa9560e568423518024e2ae09c5533
mena.samy <mena.samy@gmail.com>
2014-12-21 14:22:15 +0000

    add combo

---
+++ Combo/Helper/JsonConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class JsonConverter
+    {
+        public static T Deserialize<T>(string stringValue)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(stringValue);
+        }
+
+        public static T Deserialize<T>(string stringValue, bool withType)
+        {
+            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings { TypeNameHandling = Newtonsoft.Json.TypeNameHandling.All };
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(stringValue, settings);
+        }
+
+        public static T Deserialize<T>(string stringValue, Newtonsoft.Json.JsonSerializerSettings settings)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(stringValue, settings);
+        }
+
+        public static string Serialize(object instance)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(instance);
+        }
+
+        public static string Serialize(object instance, bool withType)
+        {
+            Newtonsoft.Json.JsonSerializerSettings settings = new Newtonsoft.Json.JsonSerializerSettings { TypeNameHandling = Newtonsoft.Json.TypeNameHandling.All };
+            return Newtonsoft.Json.JsonConvert.SerializeObject(instance, settings);
+        }
+    }
+}
