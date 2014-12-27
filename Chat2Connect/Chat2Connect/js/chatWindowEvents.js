4498e5dc49e39b9331781608c33e151bb2625330
M.Gherbas <M.Gherbas@gmail.com>
2014-12-27 14:01:40 +0000

    revamp friends list

---
+++ Chat2Connect/Chat2Connect/js/chatWindowEvents.js
@@ -0,0 +1,29 @@
+﻿function toggleBlockMember(data, event, block) {
+    var mid = data.MemberID();
+    if (block) {
+        $('#usernode-' + mid).popover('hide');
+    }
+    $.ajax({
+        url: '../Services/chat2connect.asmx/toggleBlockMember',
+        type: 'GET',
+        data: { mid: mid, block: block },
+        success: function (result) {
+            if (block) {
+                notify('success', 'تم حظر الصديق بنجاح');
+                var lst = chatVM.blockedMembers();
+                var newItem = ko.mapping.fromJS(result);
+                lst.push(newItem);
+                chatVM.blockedMembers(lst);
+            }
+            else {
+                notify('success', 'تم إلغاء حظر الصديق بنجاح');
+                var newlst = ko.utils.arrayFilter(chatVM.blockedMembers(), function (f) {
+                    return f.MemberID() != mid;
+                });
+                chatVM.blockedMembers(newlst);
+            }
+
+            $('.blocktip').tooltip();
+        }
+    });
+}
